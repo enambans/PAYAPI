3c98b30effa2984f01e310727097dde238f5455a
Enam Bansah <100935572+enambans@users.noreply.github.com>
2022-06-10 09:33:39 +0000

    Add files via upload

---
+++ finale project/main.js
@@ -0,0 +1,16 @@
+let checkbox = document.getElementById('checkbox').style.backgroundClolor = 'red';
+
+
+alert();
+
+const menuOpen = document.querySelector('#menu-open');
+const menuClose = document.querySelector('#menu-close');
+const menu = document.querySelector('#menu');
+
+menuOpen.addEventListener('click' ()) => {
+    menu.style.display = 'block'
+};
+
+menuCLose.addEventListener('click' ()) => {
+    menu.style.display = 'none'
+};
